a4de0f814f907ffe55cda2afa210ad18822cdedc
Luciano Martínez <luciano.martinez@soulit.io>
2025-03-18 21:36:25 -0300

    Demo

---
+++ StremingDemo/Controllers/VideoController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StremingDemo.Models;
+
+namespace StremingDemo.Controllers;
+
+[Route("api/videos")]
+[ApiController]
+public class VideoController : ControllerBase
+{
+    private readonly string _videoPath = "wwwroot/videos/";
+
+    [HttpGet("{fileName}")]
+    public IActionResult GetVideo(string fileName)
+    {
+        var filePath = Path.Combine(Directory.GetCurrentDirectory(), _videoPath, fileName);
+
+        if (!System.IO.File.Exists(filePath))
+            return NotFound("Video not found");
+
+        var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+        return File(stream, "video/mp4", enableRangeProcessing: true); // Enables Streming
+    }
+
+    [HttpGet("list")]
+    public IActionResult GetVideos()
+    {
+        var files = Directory.GetFiles(Path.Combine(Directory.GetCurrentDirectory(), _videoPath));
+
+        List<Video> videos = new List<Video>();
+
+        foreach (var file in files)
+        {
+            var video = new Video
+            {
+                Code = Path.GetFileName(file).Split('.')[0],
+                Name = Path.GetFileName(file)
+            };
+            videos.Add(video);
+        }
+
+
+        return Ok(videos);
+    }
+
+}
+
---
+++ StremingDemo/Models/Video.cs
@@ -0,0 +1,10 @@
+﻿namespace StremingDemo.Models
+{
+    public class Video
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Code { get; set; } = string.Empty;
+
+
+    }
+}
